90cfd87b6bf7f94d39c4503fb66ad5e1154e9765
Jim Kerver <jim.kerver@iribov.com>
2024-12-30 13:12:25 -0600

    Customize Landing Page & Social Card
    This commit changes the docusaurus logo with the 4DMethod logo in both the page and social card. As well as minor changes to titles, taglines, etc.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '4D Method',
+  tagline: 'Documentation made beautiful',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -74,12 +74,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/4DMethod.png',
       navbar: {
-        title: 'My Site',
+        title: '4D Method',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: '4D Method Logo',
+          src: 'img/4DMethod.png',
         },
         items: [
           {
